cb034eb83d5b0f9df0a1cf676616357693952152
Om Kumar <omkr1504@gamil.com>
2026-08-20 20:48:23 +0530

    Add multer middlerware

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -44,7 +44,7 @@ const userSchema =new Schema({
         type:String,
         required:[true,'Password is required']
     },
-    refreshTokens:{
+    refreshToken:{
         type:String
     }
 },{
@@ -55,7 +55,7 @@ const userSchema =new Schema({
 userSchema.pre("save", async function (next){
     if(!this.isModfied("password"))return next();
 
-    this.password=bcrypt.hash(this.password,10)
+    this.password= await bcrypt.hash(this.password,10)
     next()
 })
 
